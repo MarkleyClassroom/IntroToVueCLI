bd82ad932aaba202b4a6b98041aacfd09ee90b7c
Kirsten Markley <markles@Kirstens-Mac-mini.local>
2021-05-02 18:27:17 -0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,31 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Count msg=" the Dogs">Create a Dog</Count>
+    <Count msg=" the Cats">Create a Cat</Count>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Count from './components/Count.vue'
+import 'bootstrap'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Count
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin:0px;
+  padding:0px; 
 }
+
 </style>
---
+++ src/components/Count.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <p><slot></slot></p>
+        <p v-if="cntr>0">Counting {{cntr}} {{theMesg}}</p>
+        <button @click="addMe">Add One to Button</button>
+    </div>
+</template>
+
+
+<script>
+
+export default{
+    name: 'Count',
+    props: ['msg'],
+    data()
+    {   
+        return {
+            theMesg : this.msg,
+            cntr: 0
+        }
+    },
+    methods:
+    {
+        addMe()
+        {
+            this.theMesg = this.msg + " One more added";
+            this.cntr++
+        }
+    }
+   
+}
+</script>
+
+<style scoped>
+    div {background: linear-gradient(to left, #1f4037, #99f2c8);
+        padding: 5px; 
+        border-bottom: 2px black dotted
+    }
+
+</style>
